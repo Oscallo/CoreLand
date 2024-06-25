d96361907517e9d91b6ed024fc202bdad7893235
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2024-06-25 20:06:30 +0500

    Добавлен интерфейс обозначающий окно.

---
+++ CoreLand/Design/Windows/IWindow.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreLand.Design.Windows
+{
+    public interface IWindow
+    {
+        public double Height { get; set; }
+        
+        public double Width { get; set; }
+
+        public string Title { get; set; }
+    }
+}

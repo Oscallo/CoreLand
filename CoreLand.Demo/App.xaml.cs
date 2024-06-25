688b5609883e8863ab919ed8a89138659b7221a7
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2024-06-25 20:08:25 +0500

    Изменено название запускаемого проекта.

--- CoreLand.Demo/App.xaml.cs
+++ CoreLand.Demo/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CoreLand.WorkArea
+namespace CoreLand.Demo
 {
     /// <summary>
     /// Interaction logic for App.xaml

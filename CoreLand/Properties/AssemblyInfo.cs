f406644141b59184212e8035beb157c471eff5be
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2024-06-25 20:59:41 +0500

    Добавлен файл с аатрибутами сборки.

---
+++ CoreLand/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using CoreLand.Properties;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.oscallo.com/coreland/ui", AssemblyInfoNamespaces.WindowNamespace)]
+[assembly: XmlnsDefinition("http://schemas.oscallo.com/coreland/windows", AssemblyInfoNamespaces.WindowNamespace)]
---
+++ CoreLand/Properties/AssemblyInfoNamespaces.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreLand.Properties
+{
+    public static class AssemblyInfoNamespaces
+    {
+        public const string WindowNamespace = "CoreLand.Design.Windows";
+    }
+}
